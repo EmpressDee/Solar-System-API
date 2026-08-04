bd0fbf8a0af918eec8075edee5ef4c8eb2c8aacc
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-03 22:49:49 -0400

    changed fragment tag to div tag to make className

--- src/components/SearchPlanet.jsx
+++ src/components/SearchPlanet.jsx
@@ -28,17 +28,24 @@ function SearchPlanet() {
       setLoading(false);
     }
   }
+
+// gets first image or null
+const imageUrl = image?.collection?.items?.[0]?.links?.[0]?.href ?? null;
+
    return (
-    <>
-    <form onSubmit={handleSearch}>
+    <div className="planet-search">
+<form onSubmit={handleSearch}>
         <input value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
         <button type="submit">Search</button>
     </form>
     {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
       {facts && <h2>{facts.englishName}</h2>}
+
+    </div>
     
-    </>
+    
+    
    );
 
 
