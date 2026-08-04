6e0af5bdbb9b8d67537672f3763bc9a9cd3fc21b
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-03 21:33:01 -0400

    added try-catch logic

--- src/components/SearchPlanet.jsx
+++ src/components/SearchPlanet.jsx
@@ -8,4 +8,24 @@ function SearchPlanet() {
     const [image, setImage] = useState(null);
     const [loading, setLoading] = useState(false);x
     const [error, setError] = useState(null);
+
+    async function handleSearch(e) {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const [factsData, imageData] = await Promise.all([
+        getPlanetFacts(searchTerm),
+        getImage(searchTerm),
+      ]);
+
+      setFacts(factsData);
+      setImage(imageData);
+    } catch (err) {
+      setError("Unable to find that planet. Try search again");
+    } finally { 
+      setLoading(false);
+    }
+  }
 }
