86b7c862e0089c05efcf8fd4992704284a685d98
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-03 21:01:06 -0400

    finished image api

--- src/services/imagesApi.jsx
+++ src/services/imagesApi.jsx
@@ -1,9 +1,23 @@
 
 
- const apiKey = import.meta.env.VITE_NASA_KEY;
 
-export default async function getImage () {
 
-    const response = await fetch (``)
+export default async function getImage (searchTerm) {
 
+  try {
+    const res = await fetch(
+      `https://images-api.nasa.gov/search?q=${searchTerm}&media_type=image`
+    );
+
+    if (!res.ok) {
+      throw new Error(`Request failed: ${res.status}`);
+    }
+
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error("Failed to fetch planet image:", err.message);
+    throw err; 
+  }
 }
+   
