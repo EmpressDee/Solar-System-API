e54ea356263ab68672e3704919dfa2b5404386ee
Claudena Daniel <cjdaniel0530@gmail.com>
2026-08-06 10:37:41 -0400

    added listening port

--- backend/planetServer.js
+++ backend/planetServer.js
@@ -9,3 +9,8 @@ const app = express();
 app.use(cors());
 app.use("/api/planet", planetRoutes);
 
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => console.log (`Server is running on port ${PORT}`))
+
